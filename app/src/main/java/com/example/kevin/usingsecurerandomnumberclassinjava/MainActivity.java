2539742cbde07ee93c1f446dc993d11730b24cbf
kevin <kevinjobrien420@gmail.com>
2018-06-12 03:12:08 -0500

    189 App Number 16 - Part4 - Using SecureRandomNumber Class in Java //Coding

--- app/src/main/java/com/example/kevin/usingsecurerandomnumberclassinjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/usingsecurerandomnumberclassinjava/MainActivity.java
@@ -16,11 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final TextView txtDiceNumber1 = (TextView)findViewById(R.id.txtDiceNumber1);
-        TextView txtDiceNumber2 = (TextView)findViewById(R.id.txtDiceNumber2);
-        TextView txtDiceNumber3 = (TextView)findViewById(R.id.txtDiceNumber3);
-        TextView txtDiceNumber4 = (TextView)findViewById(R.id.txtDiceNumber4);
-        TextView txtDiceNumber5 = (TextView)findViewById(R.id.txtDiceNumber5);
-        TextView txtDiceNumber6 = (TextView)findViewById(R.id.txtDiceNumber6);
+        final TextView txtDiceNumber2 = (TextView)findViewById(R.id.txtDiceNumber2);
+        final TextView txtDiceNumber3 = (TextView)findViewById(R.id.txtDiceNumber3);
+        final TextView txtDiceNumber4 = (TextView)findViewById(R.id.txtDiceNumber4);
+        final TextView txtDiceNumber5 = (TextView)findViewById(R.id.txtDiceNumber5);
+        final TextView txtDiceNumber6 = (TextView)findViewById(R.id.txtDiceNumber6);
 
         Button btnRollTheDice = (Button)findViewById(R.id.btnRollTheDice);
 
@@ -42,9 +42,53 @@ public class MainActivity extends AppCompatActivity {
 
                 for (int faceNum = 1; faceNum < occurance.length; faceNum++) {
 
-                    if (faceNum == 1) {
+                    /*if (faceNum == 1) {
                         txtDiceNumber1.setText(occurance[faceNum] + "");
                     }
+                    if (faceNum == 2) {
+                        txtDiceNumber2.setText(occurance[faceNum] + "");
+                    }
+                    if (faceNum == 3) {
+                        txtDiceNumber3.setText(occurance[faceNum] + "");
+                    }
+                    if (faceNum == 4) {
+                        txtDiceNumber4.setText(occurance[faceNum] + "");
+                    }
+                    if (faceNum == 5) {
+                        txtDiceNumber5.setText(occurance[faceNum] + "");
+                    }
+                    if (faceNum == 6) {
+                        txtDiceNumber6.setText(occurance[faceNum] + "");
+                    }*/
+
+                    /*switch (faceNum) {
+
+                        case 1:
+                            txtDiceNumber1.setText(occurance[faceNum] + "");
+                            break;
+                        case 2:
+                            txtDiceNumber2.setText(occurance[faceNum] + "");
+                            break;
+                        case 3:
+                            txtDiceNumber3.setText(occurance[faceNum] + "");
+                            break;
+                        case 4:
+                            txtDiceNumber4.setText(occurance[faceNum] + "");
+                            break;
+                        case 5:
+                            txtDiceNumber5.setText(occurance[faceNum] + "");
+                            break;
+                        case 6:
+                            txtDiceNumber6.setText(occurance[faceNum] + "");
+                            break;
+                    }*/
+
+                    switch (faceNum) {
+                        case 1:
+
+                    }
+
+
 
                 }
             }
@@ -56,4 +100,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private void  putTheRightValueToTheTextView(TextView txtDiceNumber, int[] occurance, int index) {
+
+        txtDiceNumber.setText(occurance[index] + "");
+
+
+    }
+
+
+
+
 }
